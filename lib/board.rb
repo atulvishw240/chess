a710b1a7c62758a4a4f87aae8c92bb0dcea87adf
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-13 21:24:48 +0530

    Implement Board#print_board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,47 @@
 # BOARD
 class Board
+  WHITE = "\e[47m \e[0m".freeze
+  CYAN = "\e[46m \e[0m".freeze
+
+  attr_accessor :board
+
   def initialize
-    @board
+    @board = Array.new(8) { Array.new(8) } # 10 for simplicity
+  end
+
+  def print_board
+    board.each_with_index do |row, row_index|
+      row.each_index do |col_index|
+        print_chess_square(row_index, col_index)
+      end
+
+      puts "\n"
+    end
+  end
+
+  #----------------------------------------ALL PRIVATE METHODS ARE BELOW----------------------------------
+  private
+
+  def print_chess_square(row_index, col_index)
+    if sum(row_index, col_index).even?
+      print_cyan_square
+    else
+      print_white_square
+    end
+  end
+
+  def print_white_square
+    print WHITE + WHITE
+  end
+
+  def print_cyan_square
+    print CYAN + CYAN
+  end
+
+  def sum(row_index, col_index)
+    row_index + col_index
   end
 end
+
+board = Board.new
+board.print_board
