0ff05618047b2e3baa809dfc0c0a7d940096f3f7
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-14 08:43:20 +0530

    Create Piece#initialize

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -1,6 +1,11 @@
 # Piece
 class Piece
-  attr_accessor :player, :rank, :file
+  attr_accessor :player, :position
+  attr_reader :unicode
+
+  def initialize(unicode)
+    @unicode = unicode
+  end
 
   def move; end
   def capture; end
