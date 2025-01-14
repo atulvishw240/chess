368635445830504ea4b61c08a12e70c0c0383957
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-14 09:47:51 +0530

    Create #int_to_file

--- lib/mod_utils.rb
+++ lib/mod_utils.rb
@@ -1,7 +1,12 @@
 # Helper Methods
 module Utils
-  def files_to_int(file)
-    array = ["", "a", "b", "c", "d", "e", "f", "g", "h"]
-    array.index(file)
+  FILES = ["", "a", "b", "c", "d", "e", "f", "g", "h"].freeze
+
+  def file_to_int(file)
+    FILES.index(file)
+  end
+
+  def int_to_file(integer)
+    FILES[integer]
   end
 end
