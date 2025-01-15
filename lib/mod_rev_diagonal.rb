b9cd81d50827c68f097ae56e1ea1ae7303573036
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-15 14:03:01 +0530

    Extract reverse diagonal moves functionality into module

---
+++ lib/mod_rev_diagonal.rb
@@ -0,0 +1,28 @@
+# REVERSE DIAGONAL MOVES
+class ReverseDiagonal
+  def upper_moves_right(file, rank, stop_row = 8, stop_col = 8)
+    moves = []
+    col_index = file_to_int(file)
+    until rank == stop_row || col_index == stop_col
+      rank += 1
+      col_index += 1
+      file = int_to_file(col_index)
+      moves << [file, rank]
+    end
+
+    moves
+  end
+
+  def lower_moves_left(file, rank, stop_row = 1, stop_col = 1)
+    moves = []
+    col_index = file_to_int(file)
+    until rank == stop_row || col_index == stop_col
+      rank -= 1
+      col_index -= 1
+      file = int_to_file(col_index)
+      moves << [file, rank]
+    end
+
+    moves
+  end
+end
--- lib/mod_utils.rb
+++ lib/mod_utils.rb
@@ -9,31 +9,4 @@ module Utils
   def int_to_file(integer)
     FILES[integer]
   end
-
-  # REVERSE DIAGONAL MOVES
-  def upper_moves_right(file, rank, stop_row = 8, stop_col = 8)
-    moves = []
-    col_index = file_to_int(file)
-    until rank == stop_row || col_index == stop_col
-      rank += 1
-      col_index += 1
-      file = int_to_file(col_index)
-      moves << [file, rank]
-    end
-
-    moves
-  end
-
-  def lower_moves_left(file, rank, stop_row = 1, stop_col = 1)
-    moves = []
-    col_index = file_to_int(file)
-    until rank == stop_row || col_index == stop_col
-      rank -= 1
-      col_index -= 1
-      file = int_to_file(col_index)
-      moves << [file, rank]
-    end
-
-    moves
-  end
 end
