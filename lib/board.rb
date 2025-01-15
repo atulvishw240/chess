1ddfa8a5ab433c7a7b428e5dae728c1941a6c5e0
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-15 22:39:19 +0530

    Small refactoring

--- lib/board.rb
+++ lib/board.rb
@@ -4,22 +4,24 @@ class Board
   include Utils
 
   RESET_TERMINAL = "\e[0m".freeze
-  WHITE = "\e[47m".freeze
-  # BRIGHT_CYAN = "\e[106m".freeze
-  BRIGHT_CYAN = "\e[48;5;62m".freeze
-  DARK_YELLOW = "\e[1;33m".freeze
+  BLACK_FOREGROUND = "\e[30m".freeze
+  WHITE_BACKGROUND = "\e[47m".freeze
+  BLUE_BACKGROUND = "\e[48;5;62m".freeze
+  DARK_YELLOW_FOREGROUND = "\e[1;33m".freeze
 
-  attr_accessor :board
+  attr_accessor :board, :white_pieces, :black_pieces
 
-  def initialize
+  def initialize(white_pieces, black_pieces)
     # Ignore 0 based index for simplicity
     @board = Array.new(9) { Array.new(9, "  ") }
+    @white_pieces = white_pieces
+    @black_pieces = black_pieces
   end
 
   def print_board
     board[1..8].each_with_index do |row, row_index|
       rank = 8 - row_index
-      print DARK_YELLOW + rank.to_s + inline_space(1) + RESET_TERMINAL
+      print DARK_YELLOW_FOREGROUND + rank.to_s + inline_space(1) + RESET_TERMINAL
 
       row[1..8].each_index do |col_index|
         print_chess_square(row_index, col_index)
@@ -51,17 +53,17 @@ class Board
 
   def print_white_square(row_index, col_index)
     element = board[row_index][col_index]
-    print WHITE + element + RESET_TERMINAL
+    print WHITE_BACKGROUND + element + RESET_TERMINAL
   end
 
   def print_cyan_square(row_index, col_index)
     element = board[row_index][col_index]
-    print BRIGHT_CYAN + element + RESET_TERMINAL
+    print BLUE_BACKGROUND + element + RESET_TERMINAL
   end
 
   def print_files
     files = "a b c d e f g h"
-    puts DARK_YELLOW + inline_space(2) + files + RESET_TERMINAL
+    puts DARK_YELLOW_FOREGROUND + inline_space(2) + files + RESET_TERMINAL
   end
 
   def inline_space(number)
