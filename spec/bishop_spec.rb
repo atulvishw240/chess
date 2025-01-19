66b8e41e2498e1fbe9f69fa2266fa35b95973e6d
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-19 09:43:28 +0530

    Change coordinates from h1 to [8, 1]

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -6,28 +6,28 @@ describe Bishop do
   end
 
   describe "#all_possible_moves" do
-    it "returns all possible moves from a1" do
+    it "returns all possible moves from [1, 1]" do
       possible_moves = [[2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8]]
       moves = @bishop.all_possible_moves(1, 1)
 
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from d1" do
+    it "returns all possible moves from [1, 4]" do
       possible_moves = [[2, 5], [3, 6], [4, 7], [5, 8], [2, 3], [3, 2], [4, 1]]
       moves = @bishop.all_possible_moves(1, 4)
 
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from d8" do
+    it "returns all possible moves from [8, 4]" do
       possible_moves = [[7, 5], [6, 6], [5, 7], [4, 8], [7, 3], [6, 2], [5, 1]]
 
       moves = @bishop.all_possible_moves(8, 4)
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from e4" do
+    it "returns all possible moves from [4, 5]" do
       possible_moves = [[5, 6], [6, 7], [7, 8], [5, 4], [6, 3], [7, 2], [8, 1], [3, 6],
                         [2, 7], [1, 8], [3, 4], [2, 3], [1, 2]]
 
--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -7,35 +7,35 @@ describe King do
   end
 
   describe "#all_possible_moves" do
-    it "returns all possible moves from e4" do
+    it "returns all possible moves from [4, 5]" do
       possible_moves = [[5, 5], [3, 5], [4, 6], [4, 4], [5, 6], [5, 4], [3, 6], [3, 4]]
 
       moves = @king.all_possible_moves(4, 5)
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from a1" do
+    it "returns all possible moves from [1, 1]" do
       possible_moves = [[2, 1], [1, 2], [2, 2]]
 
       moves = @king.all_possible_moves(1, 1)
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from h8" do
+    it "returns all possible moves from [8, 8]" do
       possible_moves = [[7, 8], [8, 7], [7, 7]]
 
       moves = @king.all_possible_moves(8, 8)
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from a8" do
+    it "returns all possible moves from [8, 1]" do
       possible_moves = [[7, 1], [8, 2], [7, 2]]
 
       moves = @king.all_possible_moves(8, 1)
       expect(moves).to eq(possible_moves)
     end
 
-    it "returns all possible moves from h1" do
+    it "returns all possible moves from [1, 8]" do
       possible_moves = [[2, 8], [1, 7], [2, 7]]
 
       moves = @king.all_possible_moves(1, 8)
