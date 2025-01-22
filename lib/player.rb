20aef6441e656a117cad1337b15c469dfcf53c02
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-22 19:50:14 +0530

    Refactor Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,22 +1,8 @@
 # PLAYER
 class Player
-  attr_accessor :name, :color
+  attr_accessor :name, :pieces
 
-  def initialize(name, color)
+  def initialize(name)
     @name = name
-    @color = color
-  end
-
-  def select_piece
-    puts "Enter coordinates of piece which you want to select (Ex: e4): "
-    coordinates = gets.chomp.chars
-    file = coordinates[0]
-    rank = coordinates[1].to_i
-
-    return coordinates if coordinates.length.equal?(2) && file.between?("a", "h") && rank.between?(1, 8)
-
-    puts "\n"
-    puts "Error, Try again!!"
-    select_piece
   end
 end
