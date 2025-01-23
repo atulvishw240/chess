7ac134661f0a33a299ff355d32ebf8dc06d27912
Atul Vishwakarma <atulvishw240@gmail.com>
2025-01-23 15:49:30 +0530

    Add more tests for Board

--- lib/game.rb
+++ lib/game.rb
@@ -13,9 +13,14 @@ class Game
   end
 
   def play
-    board.setup_board
-    current_player.pieces(board.black.set)
-    opponent.pieces(board.brown.set)
+    # 1.Set up the Board
+    # 2.Assign pieces set to players
+    # 3.Player1 will get first turn
+    #   3.1 Player1 will select a piece
+    #   3.2 He'll be displayed all possible moves for that particular piece
+    #   3.3 He'll then select one of the possible move
+    #   3.4 The piece will then move to the position selected by player
+    # 4.It's Player2 turn (He'll do the same)
   end
 
   def current_player
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,11 +2,19 @@ require_relative "../lib/board"
 require_relative "../lib/square"
 
 describe Board do
-
   before(:each) do
     @board = Board.new
-    @square = Square.new
     @board.board[3][3] = @square
+  end
+
+  describe "#update" do
+    it "updates the board with either a piece or a marker" do
+      marker = "\e[90m\u{25CF}"
+      @board.update(3, 4, marker)
+      square = @board.get_square(3, 4)
+
+      expect(marker).to eq(square.element)
+    end
   end
 
   describe "#get_square" do
@@ -22,6 +30,33 @@ describe Board do
       sum = @board.sum(3, 5)
 
       expect(sum).to eq(8)
+    end
+  end
+
+  describe "#assign_color_to_square" do
+    it "assigns a color to a square, if it doesn't already have a color" do
+      white = "\e[47m"
+      square = @board.get_square(3, 5)
+      @board.assign_color_to_square(square, 8)
+
+      expect(square.color).to eq(white)
+    end
+
+    it "assigns a cyan color to square, if the sum of its coordinates is odd" do
+      cyan = "\e[48;5;45m"
+      square = @board.get_square(4, 5)
+      @board.assign_color_to_square(square, 9)
+
+      expect(square.color).to eq(cyan)
+    end
+
+    it "doesn't assign a color to a square if it already has a color" do
+      yellow = "\e[1;33m"
+      square = @board.get_square(5, 5)
+      square.color = yellow
+      @board.assign_color_to_square(square, 10)
+
+      expect(square.color).to eq(yellow)
     end
   end
 end
