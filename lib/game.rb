705592d226b1e358d19bef90873ab97789bcad37
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-05 07:16:05 +0530

    Create method to setup Board with set of pieces

--- lib/game.rb
+++ lib/game.rb
@@ -1,55 +1,44 @@
 require_relative "board"
+require_relative "modules/mod_colorable"
 
 # GAME
 class Game
+  include Colorable
+
   attr_accessor :current_player_id, :board
 
   def initialize(black, brown, player1, player2)
     @board = Board.new
     @players = [player1, player2]
     @current_player_id = 0
+
+    setup_board(black, brown)
   end
 
-  def play
-    assign_pieces_set_to_players
-    piece = let_player_select_a_piece
-    let_player_make_a_move(piece)
-    board.display
+  def setup_board(black, brown)
+    update_pieces_start_position(black.set[0..7])
+    update_pieces_start_position(brown.set[0..7])
+
+    update_board_with_pieces(black.set[0..7])
+    update_board_with_pieces(brown.set[0..7])
   end
 
-  def assign_pieces_set_to_players
-    current_player.set_of_pieces = board.brown
-    opponent.set_of_pieces = board.black
-  end
+  def update_pieces_start_position(pieces)
+    color = pieces[0].color
+    row_index = color == BLACK_FOREGROUND ? 1 : 8
 
-  def let_player_select_a_piece
-    board.display
-
-    selection = current_player.select_piece
-    square = board.get_square(selection[0], selection[1])
-    piece = square.element
-  end
-
-  def let_player_make_a_move(piece)
-    # Display possible actions for a piece
-    possible_moves = piece.all_possible_moves
-    moves = piece.useful_moves
-    captures = piece.all_possible_captures(possible_moves)
-    piece.display_markers_and_captures(moves, captures)
-
-    # Ask player to select which action he wants to perform
-    move = current_player.make_move(moves, captures)
-    move_or_capture(piece, move, captures)
-  end
-
-  def move_or_capture(piece, move, captures)
-    if captures.include?(move)
-      row_index = move[0]
-      col_index = move[1]
-      opponent.set_of_pieces.delete_piece_at(row_index, col_index)
+    pieces.each_with_index do |piece, col_index|
+      piece.position = [row_index, col_index + 1] # To offset against 1th index based Board
     end
+  end
 
-    piece.move(move)
+  def update_board_with_pieces(pieces)
+    pieces.each do |piece|
+      row_index = piece.position[0]
+      col_index = piece.position[1]
+
+      board.update(row_index, col_index, piece)
+    end
   end
 
   def current_player
