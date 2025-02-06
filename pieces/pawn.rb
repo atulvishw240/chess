b0e544a1a34f45bf778d096b65508ee2f5e6f44a
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-06 13:35:31 +0530

    Plan pawn moves

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -10,4 +10,10 @@ class Pawn < Piece
     @unicode = "\u{265F}"
     super
   end
+
+  # PAWN MOVES
+  # Pawn moves straight, only if they aren't blocked by other pieces.
+  # It can move either 1 step or 2 steps depending on where it is on the board.
+  #   1. If it's at start then it can move either 2 steps or 1 step.
+  #   2. If it's at any other place then it can move only 1 step.
 end
