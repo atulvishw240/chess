175355f37e1204befb450f2ffe037bc6bbf5c499
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-06 13:38:27 +0530

    Create pawn_spec.rb

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -1,5 +1,6 @@
 require_relative "../lib/piece"
 require_relative "../lib/modules/mod_colorable"
+require_relative "../lib/board"
 # PAWN
 class Pawn < Piece
   include Colorable
@@ -16,4 +17,15 @@ class Pawn < Piece
   # It can move either 1 step or 2 steps depending on where it is on the board.
   #   1. If it's at start then it can move either 2 steps or 1 step.
   #   2. If it's at any other place then it can move only 1 step.
+
+  def move
+    position[0] = position[0] + 1
+  end
 end
+
+pawn = Pawn.new("P")
+pawn.position = [1, 2]
+pawn.move
+pawn.move
+pawn.move
+p pawn.position
