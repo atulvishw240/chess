daa231ca638eca07413b6503334ea2cfa9e79528
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-06 12:59:05 +0530

    Delete redundant test

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -18,13 +18,4 @@ describe Piece do
       expect(square.element).to eq(" ")
     end
   end
-
-  describe "#capture" do
-    it "captures an opponent piece" do
-      @opponent_piece = Piece.new("White")
-      @piece.capture(@opponent_piece)
-
-      expect(@piece.position).to eq(@opponent_piece.position)
-    end
-  end
 end
