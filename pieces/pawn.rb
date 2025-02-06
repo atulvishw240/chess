bd54b09c58dcf2ac8885cb24153bf541fe7a2cb5
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-06 13:32:54 +0530

    Start fresh

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -8,55 +8,6 @@ class Pawn < Piece
 
   def initialize(color)
     @unicode = "\u{265F}"
-    @start = true
     super
-  end
-
-  # Moves
-  # If it's a BLACK pawn then it will march forward by (rank - 1)
-  # If it's a BROWN pawn then it will march forward by (rank + 1)
-
-  # CAPTURES
-  # If it's a BLACK pawn then it'll capture at (rank - 1, column +/- 1)
-  # If it's a BROWN pawn then it'll capture at (rank + 1, column +/- 1)
-
-  # It's easier to start with BROWN pawn, so let's do so.
-
-  # To check if the pawn is BROWN, if it isn't then it's BLACK
-
-  # FOR BROWN PAWN
-
-  def all_possible_moves
-    moves = []
-    if start
-      moves.concat(march_two_step)
-    else
-      moves.concat(march_one_step)
-    end
-
-    moves.concat(capture)
-  end
-
-  def march_one_step
-    move = []
-    rank = row
-
-    move << [rank + 1, col]
-  end
-
-  def march_two_step
-    moves = []
-    rank = row
-
-    moves << [rank + 1, col]
-    moves << [rank + 2, col]
-  end
-
-  def capture
-    move = []
-    rank = row
-
-    move << [rank + 1, col + 1]
-    move << [rank + 1, col - 1]
   end
 end
