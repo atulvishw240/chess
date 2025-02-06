3edf45ee5dc61c7a07891c25584b3217ad12d38b
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-06 12:59:17 +0530

    Calculate queen moves using bishop and rook.

--- pieces/queen.rb
+++ pieces/queen.rb
@@ -1,9 +1,12 @@
 require_relative "../lib/piece"
-require_relative "../lib/modules/mod_utils"
+require_relative "../lib/modules/mod_straight"
+require_relative "../lib/modules/mod_diagonal"
+require_relative "../lib/board"
 
 # QUEEN
 class Queen < Piece
-  include Utils
+  include Straight
+  include Diagonal
 
   def initialize(color)
     @unicode = "\u{265B}"
@@ -11,5 +14,15 @@ class Queen < Piece
   end
 
   def all_possible_moves
+    straight = rook_moves
+    diagonal = bishop_moves
+    straight.concat(diagonal)
   end
 end
+
+board = Board.new
+queen = Queen.new("Q")
+queen.position = [4, 5]
+queen.board = board
+
+p queen.all_possible_moves
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -12,10 +12,9 @@ describe Queen do
     it "returns all possible moves from [4, 5]" do
       @queen.position = [4, 5]
 
-      possible_moves = [[5, 5], [6, 5], [7, 5], [8, 5], [3, 5], [2, 5], [1, 5], # vertical
-                        [4, 6], [4, 7], [4, 8], [4, 4], [4, 3], [4, 2], [4, 1], # horizontal
-                        [5, 4], [6, 3], [7, 2], [8, 1], [3, 6], [2, 7], [1, 8], # diagonal
-                        [5, 6], [6, 7], [7, 8], [3, 4], [2, 3], [1, 2]] # reverse diagonal
+      possible_moves = [[5, 5], [6, 5], [7, 5], [8, 5], [3, 5], [2, 5], [1, 5], [4, 6], [4, 7], [4, 8], [4, 4], [4, 3],
+                        [4, 2], [4, 1], [3, 4], [2, 3], [1, 2], [5, 6], [6, 7], [7, 8], [3, 6], [2, 7], [1, 8], [5, 4],
+                        [6, 3], [7, 2], [8, 1]]
 
       moves = @queen.all_possible_moves
       expect(moves).to eq(possible_moves)
