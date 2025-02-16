60221be8fc55f944dcf54406db098a7d09eede21
Atul Vishwakarma <atulvishw240@gmail.com>
2025-02-16 12:52:45 +0530

    Refactor board_spec.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,28 +1,33 @@
 require_relative "../lib/board"
+require_relative "../lib/pieces_set"
 require_relative "../lib/square"
 
 describe Board do
   before(:each) do
-    @board = Board.new
+    @black_set = SetOfPieces.new("\e[30m")
+    @brown_set = SetOfPieces.new("\e[38;5;160m")
+    @board = Board.new(@black_set, @brown_set)
   end
 
   describe "#display" do
     it "prints the current state of board" do
       $stdout = StringIO.new
-      white = "\e[47m"
-      cyan = "\e[48;5;45m"
-      reset = "\e[0m"
       @board.display
       $stdout.rewind
 
-      rank1 = "\e[1;33m8 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank2 = "\e[1;33m7 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
-      rank3 = "\e[1;33m6 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank4 = "\e[1;33m5 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
-      rank5 = "\e[1;33m4 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank6 = "\e[1;33m3 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
-      rank7 = "\e[1;33m2 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank8 = "\e[1;33m1 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
+      yellow = "\e[1;33m"
+      reset = "\e[0m"
+      white = "\e[47m"
+      cyan = "\e[48;5;45m"
+
+      rank1 = "#{yellow}8 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank2 = "#{yellow}7 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
+      rank3 = "#{yellow}6 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank4 = "#{yellow}5 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
+      rank5 = "#{yellow}4 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank6 = "#{yellow}3 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
+      rank7 = "#{yellow}2 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank8 = "#{yellow}1 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
 
       expect($stdout.gets).to eq(rank1)
       expect($stdout.gets).to eq(rank2)
@@ -38,19 +43,23 @@ describe Board do
   describe "#update" do
     it "updates the board with either a piece or a marker" do
       $stdout = StringIO.new
+      yellow = "\e[1;33m"
+      reset = "\e[0m"
+      white = "\e[47m"
+      cyan = "\e[48;5;45m"
       marker = "\e[90m\u{25CF}"
       @board.update(5, 1, marker)
       @board.display
       $stdout.rewind
 
-      rank1 = "\e[1;33m8 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank2 = "\e[1;33m7 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
-      rank3 = "\e[1;33m6 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank4 = "\e[1;33m5 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
-      rank5 = "\e[1;33m4 \e[0m\e[47m #{marker} \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank6 = "\e[1;33m3 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
-      rank7 = "\e[1;33m2 \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\n"
-      rank8 = "\e[1;33m1 \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\e[48;5;45m   \e[0m\e[47m   \e[0m\n"
+      rank1 = "#{yellow}8 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank2 = "#{yellow}7 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
+      rank3 = "#{yellow}6 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank4 = "#{yellow}5 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
+      rank5 = "#{yellow}4 #{reset}#{white} #{marker} #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank6 = "#{yellow}3 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
+      rank7 = "#{yellow}2 #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}\n"
+      rank8 = "#{yellow}1 #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}#{cyan}   #{reset}#{white}   #{reset}\n"
 
       expect($stdout.gets).to eq(rank1)
       expect($stdout.gets).to eq(rank2)
